96f1fc301575dd42b98143262748db9df9555db5
tataudat <me@tatd.at>
2020-10-09 23:45:54 +0700

    Update make command for neovim

--- update_neovim_nightly.sh
+++ update_neovim_nightly.sh
@@ -7,10 +7,10 @@ git checkout tags/nightly
 
 # Build neovim from source with necessary config
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    make -DCMAKE_BUILD_TYPE=Release \
-         -DCMAKE_INSTALL_PREFIX=$HOME/.local/nvim
+    make CMAKE_BUILD_TYPE=RelWithDebInfo \
+         CMAKE_INSTALL_PREFIX=$HOME/.local/nvim
 elif [[ "$OSTYPE" == "darwin" ]]; then
-    make CMAKE_BUILD_TYPE=Release \
+    make CMAKE_BUILD_TYPE=RelWithDebInfo \
          CMAKE_INSTALL_PREFIX=$HOME/.local/nvim \
          MACOSX_DEPLOYMENT_TARGET=10.14 \
          DEPS_CMAKE_FLAGS="-DCMAKE_CXX_COMPILER=$(xcrun -find c++)"
